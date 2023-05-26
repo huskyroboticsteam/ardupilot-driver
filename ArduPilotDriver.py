ca9e5b00ce3e9f6859a1cf17c1174194a3a6c702
huttongrabiel <huttonthomas@icloud.com>
2023-05-25 17:44:01 -0700

    clean ArduPilotDriver

--- ArduPilotDriver.py
+++ ArduPilotDriver.py
@@ -4,38 +4,31 @@ import sys
 import asyncio
 import websockets
 import json
+import argparse
 
-# Removing this include breaks dronekit include... IDK
-# Should just run in pipenv with python3.8 or something
-from pymavlink import mavutil
 # This if is required because dronekit is not up to date for python 3.10
-# Should just run in pipenv with python3.8 or something
 if sys.version_info.major == 3 and sys.version_info.minor >= 10:
     import collections
     setattr(collections, "MutableMapping", collections.abc.MutableMapping)
 import dronekit
 
-import argparse
-
 def debug_GPS_vars():
     (port, baud, _) = get_args()
     print("Waiting for connection to {}...".format(port))
-    pixhawk = dronekit.connect(port, wait_ready=True, baud=baud)
+    ardupilot = dronekit.connect(port, wait_ready=True, baud=baud)
     print("Connection established to {}!".format(port))
 
     while True:
         time.sleep(1)
-        lat = pixhawk.location.global_frame.lat
-        lon = pixhawk.location.global_frame.lon
-        roll  = pixhawk.attitude.roll;
-        pitch  = pixhawk.attitude.pitch;
-        yaw  = pixhawk.heading;
-        # Local declination
-
+        lat = ardupilot.location.global_frame.lat
+        lon = ardupilot.location.global_frame.lon
+        roll  = ardupilot.attitude.roll;
+        pitch  = ardupilot.attitude.pitch;
+        yaw  = ardupilot.heading;
         print("lat: {}, lon: {}, roll: {}, pitch: {}, yaw: {}"
               .format(lat, lon, roll, pitch, yaw))
 
-# Returns (port, baud)
+# Returns (port, baud, debug)
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", required=True, help="Current connection port to Pixhawk")
@@ -46,25 +39,26 @@ def get_args():
 
 async def send_GPS(websocket, port, baud):
     print("Waiting for connection to {}...".format(port))
-    pixhawk = dronekit.connect(port, wait_ready=True, baud=baud)
+    ardupilot = dronekit.connect(port, wait_ready=True, baud=baud)
     print("Connection established to {}!".format(port))
 
     # From M8N documentation:
     # If unable to preform normal compass calibration "compass dance" for any reason,
     # set parameter COMPASS_ORIENT=6 (Yaw270) for proper compass orientation.
-    pixhawk.parameters["COMPASS_ORIENT"] = 6;
+    ardupilot.parameters["COMPASS_ORIENT"] = 6;
 
     # TODO: Add a handler for a terminate message or something to kill this
     while True:
         # TODO: Decide whether to keep or remove this sleep
         time.sleep(1)
-        lat = pixhawk.location.global_frame.lat
-        lon = pixhawk.location.global_frame.lon
-        roll  = pixhawk.attitude.roll;
-        pitch  = pixhawk.attitude.pitch;
-        # Yaw and heading should be equivalent
-        yaw  = pixhawk.attitude.yaw;
-        heading = pixhawk.heading;
+
+        lat = ardupilot.location.global_frame.lat
+        lon = ardupilot.location.global_frame.lon
+        roll  = ardupilot.attitude.roll;
+        pitch  = ardupilot.attitude.pitch;
+        yaw  = ardupilot.attitude.yaw;
+        heading = ardupilot.heading;
+
         asyncio.create_task(create_GPS_message(websocket, lat, lon))
         asyncio.create_task(create_IMU_message(websocket, roll, pitch, yaw))
         asyncio.create_task(create_heading_message(websocket, heading))
@@ -92,7 +86,7 @@ async def handler(websocket):
     asyncio.create_task(send_GPS(websocket, port, baud))
 
 async def async_main():
-    # FIXME: Figure out how to specify /pixhawk protocol endpoint
+    # FIXME: Figure out how to specify /ardupilot protocol endpoint
     async with websockets.serve(handler, "localhost", 3001):
         await asyncio.Future()
 
@@ -100,5 +94,5 @@ def main():
     debug_GPS_vars()
 
 if __name__ == "__main__":
-    asyncio.run(async_main())
-    #main()
+    #asyncio.run(async_main())
+    main()
