e607df4b61a7914bdbc4b8a0f0bae74f22eb13f2
Abhay D <a.deshpande012@gmail.com>
2023-06-01 16:54:59 -0700

    Use callbacks instead of polling

--- ArduPilotDriver.py
+++ ArduPilotDriver.py
@@ -1,3 +1,4 @@
+import atexit
 import time
 import sys
 import asyncio
@@ -29,8 +30,8 @@ def debug_GPS_vars():
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("port", help="Serial port connected to ardupilot")
-    parser.add_argument("-hz", "--frequency", type=float, default=0.2, help="Rate at which data is sent")
+    parser.add_argument("-p", "--port", default="/dev/ttyACM10", help="Serial port connected to ardupilot")
+    parser.add_argument("-hz", "--frequency", type=float, default=10.0, help="Rate at which data is sent")
     parser.add_argument("-b", "--baud", default=57600, type=int, help="Baud rate of serial connection")
     parser.add_argument("--debug", action="store_true", help="Prints debug info to stdout")
     return parser.parse_args()
@@ -65,35 +66,68 @@ async def async_main():
     ardupilot = dronekit.connect(args.port, wait_ready=True, baud=args.baud)
     print("Connection established to ardupilot!")
 
+    def cleanup():
+        print("Exiting!")
+        ardupilot.close()
+    atexit.register(cleanup)
+
+    curr_time = time.perf_counter()
+    last_data_send_times = {
+        "gps": curr_time,
+        "orientation": curr_time,
+        "heading": curr_time
+    }
+
     # From M8N documentation:
     # If unable to preform normal compass calibration "compass dance" for any reason,
     # set parameter COMPASS_ORIENT=6 (Yaw270) for proper compass orientation.
     ardupilot.parameters["COMPASS_ORIENT"] = 6
 
+    print("Connecting to rover server...")
     async for websocket in websockets.connect("ws://localhost:3001/ardupilot"):
+        print("Done!")
+        def gps_callback(self, _, frame):
+            curr_time = time.perf_counter()
+            if curr_time >= last_data_send_times["gps"] + 1./args.frequency:
+                if args.debug:
+                    print("GPS Fix:", ardupilot.gps_0.fix_type, frame)
+                if ardupilot.gps_0.fix_type > 1:
+                    last_data_send_times["gps"] = curr_time
+                    asyncio.run(send_gps_message(websocket, frame.lat, frame.lon))
+        
+        def orientation_callback(self, _, attitude):
+            curr_time = time.perf_counter()
+            if curr_time >= last_data_send_times["orientation"] + 1./args.frequency:
+                if args.debug:
+                    print(attitude)
+                last_data_send_times["orientation"] = curr_time
+                asyncio.run(send_orientation_message(websocket, attitude.roll, attitude.pitch, attitude.yaw))
+        
+        def heading_callback(self, _, heading):
+            curr_time = time.perf_counter()
+            if curr_time >= last_data_send_times["heading"] + 1./args.frequency:
+                if args.debug:
+                    print("Heading", heading)
+                last_data_send_times["heading"] = curr_time
+                asyncio.run(send_heading_message(websocket, heading))
+        
+        ardupilot.add_attribute_listener("location.global_frame", gps_callback)
+        ardupilot.add_attribute_listener("attitude", orientation_callback)
+        ardupilot.add_attribute_listener("heading", heading_callback)
+
         try:
             while True:
-                lat = ardupilot.location.global_frame.lat
-                lon = ardupilot.location.global_frame.lon
-                roll = ardupilot.attitude.roll
-                pitch = ardupilot.attitude.pitch
-                yaw = ardupilot.attitude.yaw
-                heading = ardupilot.heading
-
-                if args.debug:
-                    print(f"lat={lat:.5f}, lon={lon:.5f}, roll={roll:.2f}, pitch={pitch:.2f}, yaw={yaw:.2f}, heading={heading:.2f}")
-
-                await send_gps_message(websocket, lat, lon)
-                await send_orientation_message(websocket, roll, pitch, yaw)
-                await send_heading_message(websocket, heading)
-
-                await asyncio.sleep(1./args.frequency)
-        except websockets.ConnectionClosed:
+                await asyncio.sleep(1)
+        except websockets.ConnectionClosed as e:
+            print(e)
+            ardupilot.remove_attribute_listener("location.global_frame", gps_callback)
+            ardupilot.remove_attribute_listener("attitude", orientation_callback)
+            ardupilot.remove_attribute_listener("heading", heading_callback)
             continue
 
 def main():
     debug_GPS_vars()
 
 if __name__ == "__main__":
-    #asyncio.run(async_main())
-    main()
+    asyncio.run(async_main())
+    # main()
