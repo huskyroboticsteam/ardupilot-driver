3b8c10998d5a5a2b54c80e3c10f095a2a33e64dc
Abhay D <a.deshpande012@gmail.com>
2023-06-07 15:03:21 -0700

    Improve launch script

--- launch.sh
+++ launch.sh
@@ -1,3 +1,5 @@
+cd "$(dirname "$0")"
+
 if !(tmux info &> /dev/null) && tmux has-session -t ardupilot &> /dev/null; then
     echo "Ardupilot session already exists, killing it and restarting!"
     tmux kill-session -t ardupilot
