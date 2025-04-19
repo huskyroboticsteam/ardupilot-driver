5b4b466cb756cc0052dc473e1913f0916ca270e0
Isaac <isaacw1925@gmail.com>
2025-04-19 19:10:45 +0000

    fixed altitude port

--- launch.sh
+++ launch.sh
@@ -4,5 +4,8 @@ if !(tmux info &> /dev/null) && tmux has-session -t ardupilot &> /dev/null; then
     echo "Ardupilot session already exists, killing it and restarting!"
     tmux kill-session -t ardupilot
 fi
+echo "Starting new Ardupilot session"
 tmux new -s ardupilot -d
+echo "Created new session, sending command"
 tmux send -t ardupilot "./ArduPilotDriver.py$(printf \\r)"
+echo "Command sent"
