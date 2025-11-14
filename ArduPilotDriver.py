df35468891c012d5a890f7c4abc55b8c55516ea3
Isaac <isaacw1925@gmail.com>
2025-11-13 19:23:50 -0800

    Update setup script and udev rules (#5)
    * Get altitude from Pixhawk + update README
    
    Retrieve altitude from Pixhawk using the global frame. The altitude is
    relative to the mean sea level.
    
    README updated with new message
    
    * Remove accidental add back of heading + remove heading from map
    
    * Remove heading from README
    
    * added alt to GPS
    
    Changed the GPS to contain altitude data  and for removed altitude callback.
    
    * Update setup script and udev rules
    
    * fixed altitude port
    
    * bypass permissions in tmux by sudo -validate in launch script
    
    ---------
    
    Co-authored-by: huttongrabiel <huttonthomas@icloud.com>
    Co-authored-by: PatrickR <96024158+PatrickRung@users.noreply.github.com>
    Co-authored-by: Husky Robotics <uwrobots@uw.edu>

--- ArduPilotDriver.py
+++ ArduPilotDriver.py
@@ -35,7 +35,7 @@ def debug_GPS_vars():
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--port", default="/dev/ttyACM0",
+    parser.add_argument("-p", "--port", default="/dev/ttyARDUPILOT",
                         help="Serial port connected to ardupilot")
     parser.add_argument("-hz", "--frequency", type=float,
                         default=10.0, help="Rate at which data is sent")
