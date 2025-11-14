f2e9d689d7cbcf88f9f418728dadaf6e203bb391
Isaac <isaacw1925@gmail.com>
2025-11-14 03:19:16 +0000

    bypass permissions in tmux by sudo -validate in launch script

--- launch.sh
+++ launch.sh
@@ -5,6 +5,7 @@ if !(tmux info &> /dev/null) && tmux has-session -t ardupilot &> /dev/null; then
     tmux kill-session -t ardupilot
 fi
 echo "Starting new Ardupilot session"
+sudo --validate
 tmux new -s ardupilot -d
 echo "Created new session, sending command"
 tmux send -t ardupilot "./ArduPilotDriver.py$(printf \\r)"
